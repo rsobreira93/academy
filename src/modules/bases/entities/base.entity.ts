90f2885bf81a34d233d25aec219ce692eb79685b
rsobreira93 <sobreiraromuloes@gmail.com>
2022-06-22 07:42:28 -0300

    feat: add base entity

---
+++ src/modules/bases/entities/base.entity.ts
@@ -0,0 +1,22 @@
+import {
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
