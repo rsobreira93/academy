a570e1304df0009b3ed0eaf6f65174f6b30a6d66
rsobreira93 <sobreiraromuloes@gmail.com>
2022-06-23 07:55:09 -0300

    feat: add disciplines module

---
+++ src/modules/disciplines/disciplines.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+
+import {
+  NestjsQueryGraphQLModule,
+  PagingStrategies,
+} from '@nestjs-query/query-graphql';
+import { NestjsQueryTypeOrmModule } from '@nestjs-query/query-typeorm';
+import { DisciplineDTO } from './dto/discipline.dto';
+import { Discipline } from './entities/discipline.entity';
+import { CreateDisciplineInput } from './dto/create-discipline.input';
+import { UpdateDisciplineInput } from './dto/update-discipline.input';
+
+@Module({
+  imports: [
+    NestjsQueryGraphQLModule.forFeature({
+      imports: [NestjsQueryTypeOrmModule.forFeature([Discipline])],
+      resolvers: [
+        {
+          DTOClass: DisciplineDTO,
+          EntityClass: Discipline,
+          CreateDTOClass: CreateDisciplineInput,
+          UpdateDTOClass: UpdateDisciplineInput,
+          enableTotalCount: true,
+          pagingStrategy: PagingStrategies.OFFSET,
+        },
+      ],
+    }),
+  ],
+  providers: [],
+})
+export class DisciplinesModule {}
